3ac2fdcdefd46678d0edd73c9fb41c0186ad32f5
tahayk3 <tahak3@gmail.com>
2024-03-04 12:31:20 -0600

    dia-2, capitulo 1 del curso de python

---
+++ capitulo1/funcion.py
@@ -0,0 +1,5 @@
+def suma_dos_numeros(num1,num2): 
+    return num1+num2
+
+suma_final = suma_dos_numeros(5,5) 
+print(suma_final)
---
+++ capitulo1/operadores.py
@@ -0,0 +1,35 @@
+###Operadores de asignacion###
+print("..............Operadores de asignacion..............")
+a=4; b=5
+#asigna una suma 
+x=a; x+=b;  print("x+=", x) 
+#asigna una resta 
+x=a; x-=b;  print("x-=", x)
+#asigna una multiplicacion 
+x=a; x*=b;  print("x*=", x)
+#asigna una division
+x=a; x/=b;  print("x/=", x)
+#asigna el residuo    
+x=a; x%=b;  print("x%=", x)  
+#asign el cociente de la división 
+x=a; x//=b; print("x//=", x) 
+#Calcula una potencia
+x=a; x**=b; print("x**=", x) 
+
+###Operadores relacionales###
+print("..............Operadores relaciones..............")
+#Compara si son iguales
+print("==", 5==2)
+#Compara si son diferentes 
+print("!=",5!=2) 
+#Compara si a es menor a b
+print("<",5<2) 
+#Compara si a es mayor a b 
+print(">",5>2) 
+
+
+###Operadores logicos###
+print("..............Operadores logicos..............")
+print("and, ambos debe cumplirse:", True and False)
+print("or, una debe cumplirse:", True or False)
+print("not, ninguna se cumple: ", not True)
