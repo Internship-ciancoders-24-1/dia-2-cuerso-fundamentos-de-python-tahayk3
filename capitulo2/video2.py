1dd98d6e80aaca7bfc6b28f355f33567d5389711
tahayk3 <tahak3@gmail.com>
2024-03-04 13:44:51 -0600

    Dia-2, finalizado capitulo2

---
+++ capitulo2/video2.py
@@ -0,0 +1,10 @@
+platzi = 'platzi'
+print(platzi.upper())
+print(platzi.lower())
+print(platzi.find('la'))
+print(platzi.startswith('p'))
+print(platzi.startswith('x'))
+
+def my_function():
+    """ Texto de ayuda"""
+    pass
---
+++ capitulo2/video5.py
@@ -0,0 +1,6 @@
+long_word = 'ferrocarril'
+print(long_word[1:4])
+print(long_word[1:8])
+print(long_word[::-1])
+print(long_word[:8:3])
+print(long_word[::2])
